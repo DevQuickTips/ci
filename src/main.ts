120d6a2e73cf9fae60a5278c7cacb28774dbdca6
MarvinJWendt <git@marvinjwendt.com>
2021-03-22 23:10:53 +0100

    feat: update action

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,34 @@ async function run(): Promise<void> {
   try {
     const context = github.context
 
+    const {owner, repo} = context.issue
+    const issue_number = context.issue.number
+
     const token = core.getInput('repo-token', {required: true})
     const octokit = github.getOctokit(token)
     core.debug(context.issue.number.toString())
 
+
+
     await octokit.issues.createComment({
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      issue_number: context.issue.number,
+      owner,
+      repo,
+      issue_number,
       body: 'Hello, World!'
     })
+
+    let labels = octokit.issues.listLabelsOnIssue({
+      owner,
+      repo,
+      issue_number
+    })
+
+    labels.then(e => {
+      for (let eKey in e) {
+        core.info(eKey)
+      }
+    })
+
   } catch (error) {
     core.setFailed(error.message)
   }
